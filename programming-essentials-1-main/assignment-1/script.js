481053f7c849314046d9ea936a15a842063b5a17
Luca <lucajonckheere114@gmail.com>
2024-11-04 09:50:35 +0100

    nieuwe assignments ingevoegd

---
+++ programming-essentials-1-main/assignment-1/script.js
@@ -0,0 +1,15 @@
+// Select the necessary DOM elements
+const heading = document.getElementById('');
+const textInput = document.getElementById('');
+const changeTextButton = document.getElementById('');
+
+// Add an event listener to the button
+// the first argument is the event type
+// the second argument is a callback function that will be called when the event is triggered
+changeTextButton.addEventListener('click', () => {});
+
+// Function to change the heading text
+function changeHeadingText() {
+    const newText = '';
+    heading.textContent = ''; // Update the heading with the new text
+}
